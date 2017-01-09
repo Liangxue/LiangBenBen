2229115230d4c5fed78eafd22adb3e542fa6585f
liangxue <15145078036@163.com>
2017-01-09 17:38:53 +0800

    Initial Commit

---
+++ LiangBenBen/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LiangBenBen
+//
+//  Created by xue on 2017/1/9.
+//  Copyright © 2017年 liangxue. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
